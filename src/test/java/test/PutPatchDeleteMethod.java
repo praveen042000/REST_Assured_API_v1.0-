9f859cf83e4619353145a77d2c29c15456c210c2
Praveen Manoharan1 <praveen.manoharan1@expleogroup.com>
2025-05-14 15:05:55 +0530

    putMethodupdate

--- src/test/java/test/PutPatchDeleteMethod.java
+++ src/test/java/test/PutPatchDeleteMethod.java
@@ -22,9 +22,39 @@ public class PutPatchDeleteMethod {
 		System.out.println(request.toJSONString());
 		baseURI ="https://reqres.in/api";
 		given().header("content-type","application/json")
+		  .header("x-api-key", "reqres-free-v1")
 		.contentType(ContentType.JSON).accept(ContentType.JSON)
-		.body(request.toJSONString()).when().put("/users/2")
+		.body(request.toJSONString()).when().
+		put("/users/2")
 		.then().statusCode(200).log().all();
 		}
+	
+	@Test
+	public void postMethodCreateObject() {
+	    JSONObject data = new JSONObject();
+	    data.put("year", 2022);
+	    data.put("price", 1300);
+	    data.put("CPU model", "Intel i5");
+	    data.put("Hard disk size", "512 GB");
+
+	    JSONObject request = new JSONObject();
+	    request.put("name", "Dell XPS 13");
+	    request.put("data", data);
+
+	    baseURI = "https://api.restful-api.dev";
+
+	    Response response = given()
+	        .contentType(ContentType.JSON)
+	        .body(request.toJSONString())
+	    .when()
+	        .post("/objects")
+	    .then()
+	        .statusCode(200).log().all()
+	        .extract().response();
+
+	    String id = response.jsonPath().getString("id");
+	    System.out.println("New object ID: " + id);
+	}
+
 
 }
